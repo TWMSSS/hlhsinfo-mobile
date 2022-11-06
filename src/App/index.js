2dd87a85595ca82618e430398cda69931b7202df
000hen <hen20090325@gmail.com>
2022-11-06 19:47:49 +0800

    fix langing screen not disappear

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,12 @@
+import { useEffect } from "react";
+import SplashScreen from 'react-native-splash-screen';
+
 import Navigation from "../Navigation";
 
 export default Home = () => {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
+
     return <Navigation />;
 };
